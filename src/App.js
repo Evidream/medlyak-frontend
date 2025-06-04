b051af22d11cc2907544dd3d59ad962c4ec65a37
Evidream <e.v.i.dream@mail.ru>
2025-06-04 11:38:03 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>💃 Медляк</h1>
+      <p>Ты готов пригласить на танец?</p>
+    </div>
+  );
+}
+
+export default App;
