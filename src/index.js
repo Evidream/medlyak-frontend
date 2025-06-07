dbd60180baf53f1054132b349ae4b6dfce345c31
Evidream <e.v.i.dream@mail.ru>
2025-06-07 15:52:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './index.css';
+import Welcome from "./screens/Welcome";
+import UserIntro from "./screens/UserIntro";
+import QuizIntro from "./screens/QuizIntro";
+import SwipePsychology from "./screens/SwipePsychology";
 
-// Экраны
-import Welcome from './screens/Welcome';
-import UserIntro from './screens/UserIntro';
-import QuizIntro from './screens/QuizIntro';
-import SwipePsychology from './screens/SwipePsychology';
+import "./App.css"; // или './index.css', если ты используешь index.css
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/intro" element={<UserIntro />} />
+        <Route path="/user-intro" element={<UserIntro />} />
         <Route path="/quiz-intro" element={<QuizIntro />} />
-        <Route path="/block/psychology" element={<SwipePsychology />} />
-        {/* Добавляй сюда другие блоки по мере создания */}
+        <Route path="/swipe-psychology" element={<SwipePsychology />} />
       </Routes>
     </Router>
   </React.StrictMode>
