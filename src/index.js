e9375abd20aa49388c81e280f7260d8a26339f66
Evidream <e.v.i.dream@mail.ru>
2025-06-07 15:28:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import './index.css';
 import Welcome from './screens/Welcome';
-import UserInfo from './screens/UserInfo';
+import UserIntro from './screens/UserIntro';
 import QuizIntro from './screens/QuizIntro';
-import './App.css';
+import PsychologyBlock from './screens/PsychologyBlock'; // 💡 новый импорт
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/register" element={<UserInfo />} />
-        <Route path="/intro" element={<QuizIntro />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/user-intro" element={<UserIntro />} />
+        <Route path="/quiz-intro" element={<QuizIntro />} />
+        <Route path="/block/psychology" element={<PsychologyBlock />} /> {/* ✅ новый маршрут */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
